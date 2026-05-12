62c3c28da965d9a341719a248c92016d8700a3bf
jihaddrake <ringod40@gmail.com>
2026-05-12 05:32:31 -0400

    Updated the parent class(Vehicle) and 3 three child classes (moped, hovercraft, and car) again

--- src/main/java/com/pluralsight/Car.java
+++ src/main/java/com/pluralsight/Car.java
@@ -6,15 +6,50 @@ public class Car extends Vehicle {
     private boolean isElectricPowered;
     private boolean isGasPowered;
 
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public boolean isElectricPowered() {
+        return isElectricPowered;
+    }
+
+    public void setElectricPowered(boolean electricPowered) {
+        isElectricPowered = electricPowered;
+    }
+
+    public boolean isGasPowered() {
+        return isGasPowered;
+    }
+
+    public void setGasPowered(boolean gasPowered) {
+        isGasPowered = gasPowered;
+    }
+
     public Car (String name, int  numberOfPassage, int cargoCapacity,
                 int fuelCapacity,
-                String model,String vehicleType,
+                String model, String vehicleType,
                 boolean isGasPowered, boolean isElectricPowered) {
             super(name, numberOfPassage, cargoCapacity, fuelCapacity);
+
             this.model = model;
             this.vehicleType = vehicleType;
             this.isElectricPowered = true;
             this.isGasPowered = true;
+
+
     }
 
 }
--- src/main/java/com/pluralsight/Hovercraft.java
+++ src/main/java/com/pluralsight/Hovercraft.java
@@ -4,9 +4,28 @@ public class Hovercraft extends Vehicle{
     private String model;
     private String size;
 
-    public Hovercraft(String name, int numberOfCapacity, int cargoCapacity, int fuelCapacity) {
-        super(name, numberOfCapacity, cargoCapacity,fuelCapacity);
+    public String getModel() {
+        return model;
+    }
 
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public Hovercraft(String name, int numberOfCapacity,
+                      int cargoCapacity, int fuelCapacity,
+                      String model, String size) {
+        super(name, numberOfCapacity, cargoCapacity,fuelCapacity);
+        this.model = model;
+        this.size = size;
 
     }
 }
