4f0e8aea08b2df5be440b4a0d8b72dd89df577a4
jihaddrake <ringod40@gmail.com>
2026-05-12 05:34:19 -0400

    Updated the parent class(Vehicle) and 3 three child classes (moped, hovercraft, and car) again

--- src/main/java/com/pluralsight/SemiTruck.java
+++ src/main/java/com/pluralsight/SemiTruck.java
@@ -1,7 +1,43 @@
 package com.pluralsight;
 
-public class SemiTruck {
+public class SemiTruck extends Vehicle {
     protected String model;
     protected String truckType;
+    protected String cargoType;
+
+    public SemiTruck(String name, int numberOfPassage,
+                     int cargoCapacity, int fuelCapacity,
+                     String model, String truckType, String cargoType)
+    {   //Inheritance from Vehicles(Generalized blue print?)
+        super(name, numberOfPassage, cargoCapacity, fuelCapacity);
+
+        this.model = model;
+        this.truckType = truckType;
+        this.cargoType = truckType;
+    }
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getTruckType() {
+        return truckType;
+    }
+
+    public void setTruckType(String truckType) {
+        this.truckType = truckType;
+    }
+
+    public String getCargoType() {
+        return cargoType;
+    }
+
+    public void setCargoType(String cargoType) {
+        this.cargoType = cargoType;
+    }
+
 
 }
